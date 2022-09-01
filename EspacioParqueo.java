eaa87b8bcfbf18487f7456455751ef540ece5d15
Julio Lemus <rodlemus03@gmail.com>
2022-09-01 08:46:00 -0500

    Documentacion y archivos iniciales

---
+++ EspacioParqueo.java
@@ -0,0 +1,11 @@
+class EspacioParqueo { 
+    public double ancho; 
+    public double largo; 
+    public double altura; 
+    public String caracteristicas; //Si está techado o aereo 
+    
+    public EspacioParqueo ()  { 
+     
+      
+    } 
+   } 
---
+++ Estacionamiento.java
@@ -0,0 +1,14 @@
+public class Estacionamiento { 
+    public String nombre; 
+    public String dirección; 
+    public double precio; 
+    public EspacioParqueo espacio1; 
+    public EspacioParqueo espacio2; 
+    public EspacioParqueo espacio3; 
+    public EspacioParqueo espacio4; 
+    public EspacioParqueo espacio5; 
+     
+    
+    public void desplegar() { 
+    } 
+   } 
